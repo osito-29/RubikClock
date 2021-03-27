27008e6fa906feb222577ae21edc229df66e589a
Adam <jacso.adam@gmail.com>
2021-03-27 19:51:24 +0100

    Base board setup

--- src/main/java/rubikclock/RubikClockGUI.java
+++ src/main/java/rubikclock/RubikClockGUI.java
@@ -26,7 +26,7 @@ public class RubikClockGUI {
         JPanel grid = new JPanel(new GridLayout(5,5));
 //        grid.add(new JButton(), 0, 1);
 
-
+/*
 
         JPanel boardPanel = new JPanel();
 
@@ -47,10 +47,60 @@ public class RubikClockGUI {
         }
 
 
+//        JPanel grid3 = new JPanel(new GridLayout(5,5));
+*/
+
+        for (int i= 0; i<5; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if ((i == 1  || i == 3) && (j==1 || j ==3)){
+                    JButton button = new JButton();
+                    button.setText("+");
+                    button.setSize(100, 100);
+                    grid.add(button, i, j);
+                }
+                else{
+                    JTextField field = new JTextField();
+                    if ((i+j)%2 == 1){
+                        field.setVisible(false);
+                    }
+                    grid.add(field, i, j);
+                }
+            }
+        }
+
+
+/*
+        for (int i= 0; i<5; i++){
+            for (int j = 0; j<5; j++){
+                if ((i+j)%2 == 0){
+                    if ((i == 1  || i == 3) && (j==1 || j ==3)){
+                        JButton button = new JButton();
+                        grid.add(button, i, j);
+                        grid.rep
+  */
+
+//                    }
+//                    else {
+//                        JTextField field = new JTextField();
+//                        grid.add(field, i, j);
+
+//                        JButton field = new JButton();
+//                        grid.add(field, 0, 0);
+//                    }
+//                }
+//                JTextField field = new JTextField();
+//                field.setEditable(false);
+//                field.setVisible(false);
+//                grid.add(field, i, j);
+//            }
+//        }
+
+
 
 //Make frames visible
 //        frame.getContentPane().add(grid);
-        frame.getContentPane().add(grid2);
+        frame.getContentPane().add(grid);
         frame.pack();
         frame.setVisible(true);
 
