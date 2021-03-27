690e6c882170f008f3d10ce0fef15deb7e8375a6
Adam <jacso.adam@gmail.com>
2021-03-27 23:38:55 +0100

    Basic functions work

---
+++ src/main/java/rubikclock/RubikClockBoardGUI.java
@@ -0,0 +1,74 @@
+package rubikclock;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RubikClockBoardGUI {
+
+    JPanel grid = new JPanel(new GridLayout(5,5));
+    Matrix clocks = new Matrix();
+
+    public RubikClockBoardGUI(){
+
+        for (int i= 0; i<5; i++) {
+            int btncounter = 1;
+            for (int j = 0; j < 5; j++) {
+                if ((i == 1  || i == 3) && (j==1 || j ==3)){
+                    JButton button = new JButton();
+                    button.setText("+");
+                    button.setFont(new Font("Arial", Font.BOLD, 20));
+                    button.setHorizontalAlignment(SwingConstants.CENTER);
+                    button.setPreferredSize(new Dimension(80,50));
+
+                    button.setName(String.valueOf(btncounter));
+                    button.addActionListener(new ButtonListener(i, j));
+                    grid.add(button, i, j);
+                    btncounter ++;
+                }
+                else{
+                    JTextField field = new JTextField();
+                    if ((i+j)%2 == 1){
+                        field.setPreferredSize(new Dimension(80, 50));
+                        field.setVisible(false);
+                    }
+                    else{
+                        field.setPreferredSize(new Dimension(100, 70));
+                        field.setEditable(false);
+
+                        int number = clocks.getMatrix()[i/2][j/2];
+
+                        field.setText(String.valueOf(number));
+                        field.setFont(new Font("Arial", Font.BOLD, 20));
+                        field.setHorizontalAlignment(SwingConstants.CENTER);
+                    }
+                    grid.add(field, i, j);
+                }
+            }
+        }
+
+    }
+
+    class ButtonListener implements ActionListener{
+
+        private int i, j;
+        public ButtonListener(int i, int j){
+
+
+            this.i = i;
+            this.j = j;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e){
+
+            clocks.changeMatrix(i, j);
+            System.out.println("Klikk "+ "i: " + i +"j: "+ j);
+
+            System.out.println(clocks.getMatrix()[0][0]);
+        }
+    }
+
+
+}
--- src/main/java/rubikclock/RubikClockGUI.java
+++ src/main/java/rubikclock/RubikClockGUI.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 public class RubikClockGUI {
 
+
+
     private JFrame frame;
 
     public RubikClockGUI(){
@@ -24,45 +26,17 @@ public class RubikClockGUI {
         menuBar.add(exitMenu);
 
 //Create game board
-        JPanel grid = new JPanel(new GridLayout(5,5));
 
-        Random random = new Random();
-
-        for (int i= 0; i<5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if ((i == 1  || i == 3) && (j==1 || j ==3)){
-                    JButton button = new JButton();
-                    button.setText("+");
-                    button.setFont(new Font("Arial", Font.BOLD, 20));
-                    button.setHorizontalAlignment(SwingConstants.CENTER);
-                    button.setPreferredSize(new Dimension(80,50));
-                    grid.add(button, i, j);
-                }
-                else{
-                    JTextField field = new JTextField();
-                    if ((i+j)%2 == 1){
-                        field.setPreferredSize(new Dimension(80, 50));
-                        field.setVisible(false);
-                    }
-                    else{
-                        field.setPreferredSize(new Dimension(100, 70));
-                        field.setEditable(false);
-                        int randnumber = random.nextInt(12)+1;
-
-                        field.setText(String.valueOf(randnumber));
-                        field.setFont(new Font("Arial", Font.BOLD, 20));
-                        field.setHorizontalAlignment(SwingConstants.CENTER);
-                    }
-                    grid.add(field, i, j);
-                }
-            }
-        }
+        RubikClockBoardGUI board = new RubikClockBoardGUI();
 
 //Make frames visible
-//        frame.getContentPane().add(grid);
-        frame.getContentPane().add(grid);
+
+        frame.getContentPane().add(board.grid);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void Calculate() {
 
     }
 
