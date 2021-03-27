0b44b8621d76824defff3bc9dfb1c603217ac242
Adam <jacso.adam@gmail.com>
2021-03-27 18:16:26 +0100

    Initial commit

--- src/main/java/rubikclock/RubikClockGUI.java
+++ src/main/java/rubikclock/RubikClockGUI.java
@@ -1,6 +1,20 @@
 package rubikclock;
 
+import javax.swing.*;
+
 public class RubikClockGUI {
 
+    private JFrame frame;
+
+    public RubikClockGUI(){
+        frame = new JFrame("Rubikclock");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane();
+
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 
 }
