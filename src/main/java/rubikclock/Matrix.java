ecf35164901b471205da394c356449d48f04aef7
Adam <jacso.adam@gmail.com>
2021-04-03 16:57:27 +0200

    Refactor the whole program

--- src/main/java/rubikclock/Matrix.java
+++ src/main/java/rubikclock/Matrix.java
@@ -1,12 +1,17 @@
 package rubikclock;
 
-import javax.swing.*;
 import java.util.Random;
 
+/**
+ * Defines the matrix class of the clocks in the game
+ */
 public class Matrix {
     private int mtx [][] = new int[3][3];
     Random random = new Random();
 
+    /**
+     * Class constructor to create the matrix and fill it with random numbers from 1-12
+     */
     public Matrix(){
         for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
@@ -15,9 +20,16 @@ public class Matrix {
         }
     }
 
+    /**
+     * Getter of the matrix class
+     */
     public int[][] getMatrix() {
         return mtx;
     }
+
+    /**
+     * Increases the matrix values, specified by the clicked button
+     */
 
     public void changeMatrix (int i, int j) {
         mtx[i/2][j/2] = mtx[i/2][j/2]%12 +1;
@@ -25,6 +37,11 @@ public class Matrix {
         mtx[(i/2)+1][j/2] = mtx[(i/2)+1][j/2]%12 +1;
         mtx[(i/2)+1][(j/2)+1] = mtx[(i/2)+1][(j/2)+1]%12 +1;
     }
+
+    /**
+     * Method checks if all element of the matrix is 12, so the game is ended or not
+     *
+     */
 
     public boolean isFinished(){
         boolean finished = true;
--- src/main/java/rubikclock/RubikClockGUI.java
+++ src/main/java/rubikclock/RubikClockGUI.java
@@ -4,40 +4,141 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
+/**
+ * Defines class of the game gui
+ */
 public class RubikClockGUI {
 
-    public static JFrame frame;
-    public static RubikClockBoardGUI board;
+    private int counter = 0;
+    private JFrame frame;
 
-    public static JPanel boardPanel;
+    private JPanel boardPanel;
+
+    private JPanel grid = new JPanel(new GridLayout(5,5));
+    private Matrix clocks = new Matrix();
+
+    /**
+     * Method to start a new game
+     */
+    public void startNewGame(){
+        SwingUtilities.updateComponentTreeUI(frame);
+        counter = 0;
+        frame.getContentPane().remove(grid);
+        clocks = new Matrix();
+        this.refreshGame();
+        frame.getContentPane().add(grid);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Method to update game after an event (click on button) happens
+     */
+    public void refreshGame(){
+
+        this.grid = new JPanel(new GridLayout(5,5));
+
+        for (int i= 0; i<5; i++) {
+            int btncounter = 1;
+            for (int j = 0; j < 5; j++) {
+                if ((i == 1  || i == 3) && (j==1 || j ==3)){
+                    JButton button = new JButton();
+                    button.setText("+");
+                    button.setFont(new Font("Arial", Font.BOLD, 20));
+                    button.setHorizontalAlignment(SwingConstants.CENTER);
+                    button.setPreferredSize(new Dimension(80,50));
+
+                    button.setName(String.valueOf(btncounter));
+                    button.addActionListener(new ButtonListener(i, j));
+                    this.grid.add(button, i, j);
+                    btncounter ++;
+                }
+                else{
+                    JTextField field = new JTextField();
+                    if ((i+j)%2 == 1){
+                        field.setPreferredSize(new Dimension(80, 50));
+                        field.setVisible(false);
+                    }
+                    else{
+                        field.setPreferredSize(new Dimension(100, 70));
+                        field.setEditable(false);
+
+                        int number = clocks.getMatrix()[i/2][j/2];
+
+                        field.setText(String.valueOf(number));
+                        field.setFont(new Font("Arial", Font.BOLD, 20));
+                        field.setHorizontalAlignment(SwingConstants.CENTER);
+                    }
+                    this.grid.add(field, i, j);
+                }
+            }
+        }
+    }
+
+    /**
+     * Method to implement event action in case of a button click
+     */
+
+    public class ButtonListener implements ActionListener {
+
+        private int i, j;
+        public ButtonListener(int i, int j){
+
+            this.i = i;
+            this.j = j;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e){
+
+            frame.getContentPane().remove(grid);
+            clocks.changeMatrix(i, j);
+            refreshGame();
+            frame.getContentPane().add(grid);
+            SwingUtilities.updateComponentTreeUI(frame);
+            counter++;
+
+
+            if(clocks.isFinished()){
+                JOptionPane.showMessageDialog(boardPanel, "Congratulations! You won in " +counter+" steps!","Congratulations!", JOptionPane.PLAIN_MESSAGE);
+                startNewGame();
+
+            }
+
+        }
+    }
+
+    /**
+     * Constructor of the game gui
+     */
 
     public RubikClockGUI(){
 
-//Create frame elements
+        /**
+         * Creates frame elements
+         */
         frame = new JFrame("Rubikclock");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        /**
+         * Creates menu items
+         */
 
-//Create menu items
         JMenuBar menuBar = new JMenuBar();
         frame.setJMenuBar(menuBar);
         JMenuItem newgameMenu = new JMenuItem("New game");
         menuBar.add(newgameMenu);
-
         newgameMenu.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                frame.remove(board.grid);
-                board = new RubikClockBoardGUI();
-                board.clocks = new Matrix();
-                frame.getContentPane().add(board.grid);
                 SwingUtilities.updateComponentTreeUI(frame);
-                frame.pack();
-                frame.setVisible(true);
+                startNewGame();
             }
         });
+
+        /**
+         * Sets exit menu item
+         */
 
         JMenuItem exitMenu = new JMenuItem("Exit");
         menuBar.add(exitMenu);
@@ -49,51 +150,12 @@ public class RubikClockGUI {
             }
         });
 
+        /**
+         * Method call to start a new game
+         */
 
+        this.startNewGame();
 
-//Create game board
-
-        board = new RubikClockBoardGUI();
-
-//Make frames visible
-
-        frame.getContentPane().add(board.grid);
-        frame.pack();
-        frame.setVisible(true);
-
-//Setup exit button action
-
-    }
-
-
-    static class ButtonListener implements ActionListener {
-
-        private int i, j;
-        public ButtonListener(int i, int j){
-
-            this.i = i;
-            this.j = j;
-        }
-        @Override
-        public void actionPerformed(ActionEvent e){
-
-            RubikClockBoardGUI.clocks.changeMatrix(i, j);
-
-            frame.remove(board.grid);
-
-            board = new RubikClockBoardGUI();
-
-            frame.getContentPane().add(board.grid);
-
-            SwingUtilities.updateComponentTreeUI(frame);
-
-//            board.clocks.isFinished() = true;
-
-            if(true){
-                JOptionPane.showMessageDialog(boardPanel, "Congartulations! You won the game!","Congratulations!", JOptionPane.PLAIN_MESSAGE);
-            }
-
-        }
     }
 
 }
